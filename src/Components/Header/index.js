3f2724925a177a6ded11a4772ebafce192e9e5cc
edilsonnog <edilson.n@hotmail.com>
2021-05-19 20:29:19 -0300

    Layout

---
+++ src/Components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+
+function Header() {
+    return (
+        <header>
+            <a href="">Login</a>
+            <a href="">Home</a>
+            <a href="">Cadastre-se</a>
+        </header>
+    )
+}
+
+export default Header;
